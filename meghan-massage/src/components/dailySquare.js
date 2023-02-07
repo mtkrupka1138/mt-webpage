1bd85f39ef6c84930145d690c1d7212ebd7a1b41
Meghan Krupka <mkrupka.hdm@gmail.com>
2023-02-06 19:25:14 -0500

    functions for squares

---
+++ meghan-massage/src/components/dailySquare.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function dailySquare() {
+    const [squares, setSquares] = React.useState([])
+    const [currentSquareId, setCurrentSquareId] = React.useState(
+        (squares[0] && squares[0].id) || ""
+    )
+
+
+function createNewSquare() {
+    const newSquare = {
+
+    }
+}
+
+function updateSquare(text) {
+
+}
+
+return (
+    
+)
+
+}
